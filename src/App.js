7b21212648f81a50af5fa20394a80845eccc8931
Alexey Rykov <rykov311@gmail.com>
2022-08-02 22:52:01 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import Total from "./components/total/Total";
+import Operation from "./components/operation/Operation";
+import History from "./components/history/History";
+
+
+class App extends Component {
+
+    state = {
+        transactions: JSON.parse(localStorage.getItem('calcMoney')) || [],
+        description: '',
+        amount: '',
+        resultIncome: 0,
+        resultExpenses: 0,
+        totalBalance: 0,
+    }
+
+    componentDidMount() {
+        this.getTotalBalance();
+    };
+
+    componentDidUpdate() {
+        this.addStorage();
+    };
+
+    addTransaction = add => {
+
+        const transactions = [...this.state.transactions];
+
+        const transaction = {
+            id: `cmr${(+new Date).toString(16)}`,
+            description: this.state.description,
+            amount: parseFloat(this.state.amount),
+            add
+        };
+        transactions.push(transaction)
+        this.setState({
+            transactions,
+            description: '',
+            amount: '',
+        }, this.getTotalBalance);
+    };
+
+    addAmount = e => {
+        this.setState({amount: e.target.value})
+    }
+
+    addDescription = e => {
+        this.setState({description: e.target.value})
+    }
+
+    getIncome = () => this.state.transactions
+        .reduce((acc, item) => item.add ? acc + item.amount : acc, 0)
+
+    getExpenses = () => this.state.transactions
+        .reduce((acc, item) => !item.add ? acc + item.amount : acc, 0)
+
+
+    getTotalBalance() {
+        const resultIncome = this.getIncome();
+        const resultExpenses = this.getExpenses();
+
+        const totalBalance = resultIncome - resultExpenses;
+        this.setState({
+            resultIncome,
+            resultExpenses,
+            totalBalance,
+        });
+    };
+
+    addStorage() {
+        localStorage.setItem('calcMoney', JSON.stringify(this.state.transactions))
+    };
+
+    delTransaction = key => {
+        const transactions = this.state.transactions.filter(item => item.id !== key);
+        this.setState({ transactions }, this.getTotalBalance)
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <header className="header">
+                    <h1>Кошелек</h1>
+                    <h2>Калькулятор расходов</h2>
+                </header>
+                <main>
+
+                    <div className="container">
+                        <Total resultIncome={this.state.resultIncome}
+                               resultExpenses={this.state.resultExpenses}
+                               totalBalance={this.state.totalBalance}
+                        />
+                        <History transactions={this.state.transactions}
+                                 delTransaction={this.delTransaction}
+                        />
+                        <Operation addTransaction={this.addTransaction}
+                                   addAmount={this.addAmount}
+                                   addDescription={this.addDescription}
+                                   description={this.state.description}
+                                   amount={this.state.amount}
+                        />
+                    </div>
+                </main>
+            </React.Fragment>
+        )
+    }
+}
+
+export default App;
